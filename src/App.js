0ebe15136247ba3c61f12d490781cd53f5fdeb7a
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-12-07 14:46:25 -0500

    added data, basic styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import initialData from "./initial-data";
+import Column from "./components/Column/Column.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+      return <Column key={column.id} column={column} tasks={tasks} />;
+    });
+  }
 }
-
-export default App;
