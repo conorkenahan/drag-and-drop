c65fa5182e8cd710f8c8e1d7cf2a07ae68b6d7e6
Conor <67439130+conorkenahan@users.noreply.github.com>
2020-12-14 22:10:40 -0500

    added handle to draggable

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,11 +3,20 @@ import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
+  display: flex;
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
   background-color: ${(props) => (props.isDragging ? "lightgreen" : "white")};
+`;
+
+const Handle = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: orange;
+  border-radius: 4px;
+  margin-right: 8px;
 `;
 
 export default class Task extends React.Component {
@@ -21,6 +30,7 @@ export default class Task extends React.Component {
             ref={provided.innerRef}
             isDragging={snapshot.isDragging}
           >
+            <Handle {...provided.dragHandleProps} />
             {this.props.task.content}
           </Container>
         )}
